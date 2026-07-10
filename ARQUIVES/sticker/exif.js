d2f842b2f9c39003d836178e4600ca934bbef264
Sr-agente208 <sr.agente208@gmail.com>
2026-07-10 14:20:36 -0300

    Fix: Corrigir writeExifImg e writeExifVid para retornar buffer em vez de filepath

--- ARQUIVES/sticker/exif.js
+++ ARQUIVES/sticker/exif.js
@@ -21,7 +21,7 @@ async function imageToWebp (media) {
                 "-vcodec",
                 "libwebp",
                 "-vf",
-                "scale='min(320,iw)':min'(320,ih)':force_original_aspect_ratio=decrease,fps=15, pad=320:320:-1:-1:color=white@0.0, split [a][b]; [a] palettegen=reserve_transparent=on:transparency_color=ffffff [p]; [b][p] paletteuse"
+                "scale='min(320,iw)':min'(320,ih)':force_original_aspect_ratio=decrease,fps=15, pad=320:320:-1:-1:color=white@0.0, split [a][b]; [a] palettegen=reserve_transparent=on:transparency_ncolors=255 [p]; [b][p] paletteuse"
             ])
             .toFormat("webp")
             .save(tmpFileOut)
@@ -48,7 +48,7 @@ async function videoToWebp (media) {
                 "-vcodec",
                 "libwebp",
                 "-vf",
-                "scale='min(320,iw)':min'(320,ih)':force_original_aspect_ratio=decrease,fps=15, pad=320:320:-1:-1:color=white@0.0, split [a][b]; [a] palettegen=reserve_transparent=on:transparency_color=ffffff [p]; [b][p] paletteuse",
+                "scale='min(320,iw)':min'(320,ih)':force_original_aspect_ratio=decrease,fps=15, pad=320:320:-1:-1:color=white@0.0, split [a][b]; [a] palettegen=reserve_transparent=on:transparency_ncolors=255 [p]; [b][p] paletteuse",
                 "-loop",
                 "0",
                 "-ss",
@@ -79,16 +79,29 @@ async function writeExifImg (media, metadata) {
 
     if (metadata.packname || metadata.author) {
         const img = new webp.Image()
-        const json = { "sticker-pack-id": `https://github.com/DikaArdnt/Hisoka-Morou`, "sticker-pack-name": metadata.packname, "sticker-pack-publisher": metadata.author, "emojis": metadata.categories ? metadata.categories : [""] }
+        const json = { 
+            "sticker-pack-id": `https://github.com/DikaArdnt/Hisoka-Morou`, 
+            "sticker-pack-name": metadata.packname || 'Sticker Pack', 
+            "sticker-pack-publisher": metadata.author || 'Keisen Bot', 
+            "emojis": metadata.category || ['😀']
+        }
         const exifAttr = Buffer.from([0x49, 0x49, 0x2A, 0x00, 0x08, 0x00, 0x00, 0x00, 0x01, 0x00, 0x41, 0x57, 0x07, 0x00, 0x00, 0x00, 0x00, 0x00, 0x16, 0x00, 0x00, 0x00])
         const jsonBuff = Buffer.from(JSON.stringify(json), "utf-8")
         const exif = Buffer.concat([exifAttr, jsonBuff])
         exif.writeUIntLE(jsonBuff.length, 14, 4)
-        await img.load(tmpFileIn)
+        
+        return img.load(tmpFileIn).then(() => {
+            fs.unlinkSync(tmpFileIn)
+            img.exif = exif
+            return img.save(tmpFileOut).then(() => {
+                const buffer = fs.readFileSync(tmpFileOut)
+                fs.unlinkSync(tmpFileOut)
+                return buffer
+            })
+        })
+    } else {
         fs.unlinkSync(tmpFileIn)
-        img.exif = exif
-        await img.save(tmpFileOut)
-        return tmpFileOut
+        return wMedia
     }
 }
 
@@ -100,16 +113,29 @@ async function writeExifVid (media, metadata) {
 
     if (metadata.packname || metadata.author) {
         const img = new webp.Image()
-        const json = { "sticker-pack-id": `https://github.com/DikaArdnt/Hisoka-Morou`, "sticker-pack-name": metadata.packname, "sticker-pack-publisher": metadata.author, "emojis": metadata.categories ? metadata.categories : [""] }
+        const json = { 
+            "sticker-pack-id": `https://github.com/DikaArdnt/Hisoka-Morou`, 
+            "sticker-pack-name": metadata.packname || 'Sticker Pack', 
+            "sticker-pack-publisher": metadata.author || 'Keisen Bot', 
+            "emojis": metadata.category || ['😀']
+        }
         const exifAttr = Buffer.from([0x49, 0x49, 0x2A, 0x00, 0x08, 0x00, 0x00, 0x00, 0x01, 0x00, 0x41, 0x57, 0x07, 0x00, 0x00, 0x00, 0x00, 0x00, 0x16, 0x00, 0x00, 0x00])
         const jsonBuff = Buffer.from(JSON.stringify(json), "utf-8")
         const exif = Buffer.concat([exifAttr, jsonBuff])
         exif.writeUIntLE(jsonBuff.length, 14, 4)
-        await img.load(tmpFileIn)
+        
+        return img.load(tmpFileIn).then(() => {
+            fs.unlinkSync(tmpFileIn)
+            img.exif = exif
+            return img.save(tmpFileOut).then(() => {
+                const buffer = fs.readFileSync(tmpFileOut)
+                fs.unlinkSync(tmpFileOut)
+                return buffer
+            })
+        })
+    } else {
         fs.unlinkSync(tmpFileIn)
-        img.exif = exif
-        await img.save(tmpFileOut)
-        return tmpFileOut
+        return wMedia
     }
 }
 
@@ -121,16 +147,29 @@ async function writeExif (media, metadata) {
 
     if (metadata.packname || metadata.author) {
         const img = new webp.Image()
-        const json = { "sticker-pack-id": `https://github.com/DikaArdnt/Hisoka-Morou`, "sticker-pack-name": metadata.packname, "sticker-pack-publisher": metadata.author, "emojis": metadata.categories ? metadata.categories : [""] }
+        const json = { 
+            "sticker-pack-id": `https://github.com/DikaArdnt/Hisoka-Morou`, 
+            "sticker-pack-name": metadata.packname || 'Sticker Pack', 
+            "sticker-pack-publisher": metadata.author || 'Keisen Bot', 
+            "emojis": metadata.category || ['😀']
+        }
         const exifAttr = Buffer.from([0x49, 0x49, 0x2A, 0x00, 0x08, 0x00, 0x00, 0x00, 0x01, 0x00, 0x41, 0x57, 0x07, 0x00, 0x00, 0x00, 0x00, 0x00, 0x16, 0x00, 0x00, 0x00])
         const jsonBuff = Buffer.from(JSON.stringify(json), "utf-8")
         const exif = Buffer.concat([exifAttr, jsonBuff])
         exif.writeUIntLE(jsonBuff.length, 14, 4)
-        await img.load(tmpFileIn)
+        
+        return img.load(tmpFileIn).then(() => {
+            fs.unlinkSync(tmpFileIn)
+            img.exif = exif
+            return img.save(tmpFileOut).then(() => {
+                const buffer = fs.readFileSync(tmpFileOut)
+                fs.unlinkSync(tmpFileOut)
+                return buffer
+            })
+        })
+    } else {
         fs.unlinkSync(tmpFileIn)
-        img.exif = exif
-        await img.save(tmpFileOut)
-        return tmpFileOut
+        return wMedia
     }
 }
 
